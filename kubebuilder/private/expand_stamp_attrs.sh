98e9187a660b4a9d78202c0d7a69ff858df9b1da
Jan Hicken <janhicken@users.noreply.github.com>
2025-05-28 13:27:18 +0200

    Remove fallthrough case in expand_stamp_attrs.sh

--- kubebuilder/private/expand_stamp_attrs.sh
+++ kubebuilder/private/expand_stamp_attrs.sh
@@ -1,25 +1,19 @@
 #!/usr/bin/env bash
 set -o errexit -o nounset -o pipefail
 
-case $# in
-3)
-	info_file=$3
-	;&
-2)
-	src_file=$1
-	out_file=$2
-	;;
-*)
-	printf 'Usage: %s SOURCE_FILE INFO_FILE OUTPUT_FILE\n' "$0" >&2
+if [[ "$#" -lt 2 ]]; then
+	printf 'Usage: %s SOURCE_FILE OUTPUT_FILE [INFO_FILE]... \n' "$0" >&2
 	exit 1
-	;;
-esac
+fi
+src_file=$1
+out_file=$2
+shift 2
 
 # Export info pairs as environment variables
-if [[ -n "${info_file:-}" ]]; then
+for info_file in "$@"; do
 	while read -r key value; do
 		declare -x "$key"="$value"
 	done <"$info_file"
-fi
+done
 
 "$YQ_BIN" '(.. | select(tag == "!!str")) |= envsubst' "$src_file" >"$out_file"
