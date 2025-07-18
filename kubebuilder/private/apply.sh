50e4b4ae8055185e5cc523f71233d9ae46621184
Jan Hicken <janhicken@users.noreply.github.com>
2025-07-18 21:22:21 +0200

    Make apply.sh more robust
    The script failed with an error when only CRDs were applied. This has
    been fixed.
    
    Also, when applying a single CRD only, it is now properly waited for.
    This has not been the case previously.

--- kubebuilder/private/apply.sh
+++ kubebuilder/private/apply.sh
@@ -14,13 +14,20 @@ readonly yq_bin=%yq_bin%
 # ╔════════════════════════════════════════════════════════════════════════════╗
 # ║                                    Run                                     ║
 # ╚════════════════════════════════════════════════════════════════════════════╝
+crd_resources=$(mktemp)
+non_crd_resources=$(mktemp)
+trap 'rm -f "$crd_resources" "$non_crd_resources"' EXIT
 
 # Apply CRDs first and wait for them to be established
-"$yq_bin" 'select(.kind == "CustomResourceDefinition")' "$manifests_file" |
-	"$kubectl_bin" apply --filename=- --output=json --server-side=true |
-	"$yq_bin" '.items[].metadata.name' |
-	xargs -r -I '{crdname}' "$kubectl_bin" wait --for=condition=Established crd/'{crdname}'
+"$yq_bin" 'select(.kind == "CustomResourceDefinition")' "$manifests_file" >"$crd_resources"
+if [[ -s "$crd_resources" ]]; then
+	"$kubectl_bin" apply --filename="$crd_resources" --output=json --server-side=true |
+		"$yq_bin" '.items[].metadata.name // .metadata.name' |
+		xargs -I '{crdname}' "$kubectl_bin" wait --for=condition=Established crd/'{crdname}'
+fi
 
 # Apply other non-CRD resources
-"$yq_bin" 'select(.kind != "CustomResourceDefinition")' "$manifests_file" |
-	"$kubectl_bin" apply --filename=-
+"$yq_bin" 'select(.kind != "CustomResourceDefinition")' "$manifests_file" >"$non_crd_resources"
+if [[ -s "$non_crd_resources" ]]; then
+	"$kubectl_bin" apply --filename="$non_crd_resources"
+fi
